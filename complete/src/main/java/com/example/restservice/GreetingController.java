9bf6e05b7980b945c88e106b2f8b1ccf99bb9f0a
Ricardo Ortiz <misterlroa@gmail.com>
2021-03-13 13:18:52 -0700

    CB-291 - Added new endpoint to parse CSV file

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -1,7 +1,10 @@
 package com.example.restservice;
 
+import java.io.FileReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
-
+import com.opencsv.CSVReader;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +17,21 @@ public class GreetingController {
 
 	@GetMapping("/greeting")
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+		return new Greeting("Hola", String.format(template, name));
+	}
+
+	@GetMapping("/parseTxt")
+	public Greeting parseTxt() {
+		String id = "0";
+		String name = "";
+		try (CSVReader reader = new CSVReader(new FileReader("Names.csv"))) {
+		      List<String[]> r = reader.readAll();
+		      id =   r.get(0)[0];
+		      name = r.get(0)[1];
+		      r.forEach(x -> System.out.println(Arrays.toString(x)));
+		  }catch(Exception e) {
+			  System.out.println(e.getMessage());
+		  }
+		return new Greeting(id, String.format(template, name));
 	}
 }
